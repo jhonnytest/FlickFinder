31e057f573893fa3efa27050eb961dcbc6cc067d
Jhonny Test <iriyaz93@gmail.com>
2015-10-18 02:20:38 +0530

    Initial Commit

---
+++ FlickFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickFinder
+//
+//  Created by Riyaz Ahamed on 18/10/15.
+//  Copyright (c) 2015 SimplyApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
